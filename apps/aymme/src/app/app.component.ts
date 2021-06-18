f29201493c34a6d0c651f654361348e710fd7c7b
Nick Thesing <nickt@backbase.com>
2021-06-18 12:02:57 +0200

    Initial NX project

---
+++ apps/aymme/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+import { PrimeNGConfig } from 'primeng/api';
+
+@Component({
+  selector: 'aymme-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit {
+
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  ngOnInit() {
+      this.primengConfig.ripple = true;
+  }
+}
