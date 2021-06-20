1c46eaa3fbf9cacddb1e99751c8ff6535d019248
Petar Blazhevski <pblazhevski@mobiquityinc.com>
2021-06-20 22:28:04 +0200

    add database configuration
    Not tested yet

--- libs/api/core/feature/src/lib/api-core-feature.module.ts
+++ libs/api/core/feature/src/lib/api-core-feature.module.ts
@@ -1,13 +1,26 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getMetadataArgsStorage } from 'typeorm';
 
 import databaseConfig from './config/database.config';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    load: [databaseConfig],
-    isGlobal: true,
-
-  })],
+  imports: [
+    ConfigModule.forRoot({
+      load: [databaseConfig],
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'sqlite',
+        database: configService.get<string>('database.name'),
+        synchronize: configService.get<boolean>('database.synchronize'),
+        entities: getMetadataArgsStorage().tables.map((tbl) => tbl.target),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class ApiCoreFeatureModule {}
