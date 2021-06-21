901131df70fb3e776d881cce6541639776798882
Petar Blazhevski <pblazhevski@mobiquityinc.com>
2021-06-21 23:11:07 +0200

    fix(api): remove unused interface

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { Message } from '@aymme/api-interfaces';
 
 @Injectable()
 export class AppService {
-  getData(): Message {
+  getData() {
     return { message: 'Welcome to api!' };
   }
 }
