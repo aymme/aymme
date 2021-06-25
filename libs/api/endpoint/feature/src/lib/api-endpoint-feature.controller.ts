86ebb6f6b14199309bcfa89ddad3efa6674a165b
Petar Blazhevski <pblazhevski@mobiquityinc.com>
2021-06-25 16:49:00 +0200

    fix(api): wrong way of getting the project ID for each endpoints

--- libs/api/endpoint/feature/src/lib/api-endpoint-feature.controller.ts
+++ libs/api/endpoint/feature/src/lib/api-endpoint-feature.controller.ts
@@ -5,37 +5,33 @@ import {
   Logger,
   Param,
   ParseUUIDPipe,
-  UseGuards,
 } from '@nestjs/common';
 import { EndpointService } from '@aymme/api/endpoint/data-access';
-import { CurrentProject, ProjectGuard } from '@aymme/api/project/utils';
-import { Project } from '@aymme/api/shared/data-access';
 
-@Controller('endpoints')
-@UseGuards(ProjectGuard)
+@Controller(':projectId/endpoints')
 export class ApiEndpointFeatureController {
   private logger = new Logger(ApiEndpointFeatureController.name);
 
   constructor(private endpointService: EndpointService) {}
 
   @Get()
-  async getAll(@CurrentProject() project: Project) {
-    return this.endpointService.getAll(project.id);
+  async getAll(@Param('projectId', new ParseUUIDPipe()) projectId: string) {
+    return this.endpointService.getAll(projectId);
   }
 
   @Get(':id')
   async getById(
-    @CurrentProject() project: Project,
+    @Param('projectId', new ParseUUIDPipe()) projectId: string,
     @Param('id', new ParseUUIDPipe()) id: string
   ) {
-    return this.endpointService.getById(project.id, id);
+    return this.endpointService.getById(projectId, id);
   }
 
   @Delete(':id')
   async delete(
-    @CurrentProject() project: Project,
+    @Param('projectId', new ParseUUIDPipe()) projectId: string,
     @Param('id', new ParseUUIDPipe()) id: string
   ) {
-    return this.endpointService.delete(project.id, id);
+    return this.endpointService.delete(projectId, id);
   }
 }
--- libs/api/endpoint/feature/src/lib/api-endpoint-feature.module.ts
+++ libs/api/endpoint/feature/src/lib/api-endpoint-feature.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 
 import { ApiEndpointFeatureController } from './api-endpoint-feature.controller';
 import { ApiEndpointDataAccessModule } from '@aymme/api/endpoint/data-access';
-import { ApiProjectDataAccessModule } from '@aymme/api/project/data-access';
 
 @Module({
-  imports: [ApiEndpointDataAccessModule, ApiProjectDataAccessModule],
+  imports: [ApiEndpointDataAccessModule],
   controllers: [ApiEndpointFeatureController],
   providers: [],
   exports: [],
