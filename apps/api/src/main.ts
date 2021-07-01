ef5d3b808e070765f14e90021bc3f28edf5c11a2
Petar Blazhevski <pblazhevski@mobiquityinc.com>
2021-07-01 14:45:07 +0200

    chore(api): enable object transform in global validation pipe

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
