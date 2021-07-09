95fb8100c19ea20439943b458136288cda4d986d
Hassan Khousheish <hassan@backbase.com>
2021-07-09 16:19:14 +0200

    Fix Theme Switcher

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-
+import { ThemeSwitcherService } from 'libs/client/shell/ui/theme-switcher/src/lib/theme-switcher.service';
 import { PrimeNGConfig } from 'primeng/api';
+
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ay-root',
-  template: `<router-outlet></router-outlet>`,
   styleUrls: ['./app.component.scss'],
+  template: `<router-outlet></router-outlet>`,
 })
 export class AppComponent implements OnInit {
-  constructor(private primengConfig: PrimeNGConfig) {}
+  constructor(
+    private readonly primengConfig: PrimeNGConfig,
+    private readonly themeSwitcherService: ThemeSwitcherService,
+  ) {
+  }
 
   ngOnInit() {
     this.primengConfig.ripple = true;
+    this.themeSwitcherService.initializeTheme();
   }
 }
