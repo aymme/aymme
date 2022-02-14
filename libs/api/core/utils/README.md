0fdebd6aee76403520eddd4923576d3c17781a69
Oleg Chekryhin <Oleh.X.Chekryhin@aib.ie>
2022-02-14 13:52:04 +0100

    Update names

--- libs/api/core/utils/README.md
+++ libs/api/core/utils/README.md
@@ -1,7 +1,7 @@
-# api-project-utils
+# api-utils
 
 This library was generated with [Nx](https://nx.dev).
 
 ## Running unit tests
 
-Run `nx test api-project-utils` to execute the unit tests via [Jest](https://jestjs.io).
+Run `nx test api-utils` to execute the unit tests via [Jest](https://jestjs.io).
--- libs/api/core/utils/jest.config.js
+++ libs/api/core/utils/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'api-project-utils',
+  displayName: 'api-utils',
   preset: '../../../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -11,5 +11,5 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../../coverage/libs/api/project/utils',
+  coverageDirectory: '../../../../coverage/libs/api/core/utils',
 };
