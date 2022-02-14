c7084e952ff77dabea357ffad533cfc561b33a7d
Oleg Chekryhin <Oleh.X.Chekryhin@aib.ie>
2022-02-14 13:48:58 +0100

    Move error handling to core module

--- libs/api/project/feature/src/lib/api-project-feature.module.ts
+++ libs/api/project/feature/src/lib/api-project-feature.module.ts
@@ -1,20 +1,13 @@
 import { Module } from '@nestjs/common';
-import { APP_FILTER } from '@nestjs/core';
 
 import { ApiProjectDataAccessModule } from '@aymme/api/project/data-access';
-import { HttpExceptionFilter } from '@aymme/api/project/utils';
 
 import { ApiProjectFeatureController } from './api-project-feature.controller';
 
 @Module({
   imports: [ApiProjectDataAccessModule],
   controllers: [ApiProjectFeatureController],
-  providers: [
-    {
-      provide: APP_FILTER,
-      useClass: HttpExceptionFilter,
-    },
-  ],
+  providers: [],
   exports: [],
 })
 export class ApiProjectFeatureModule {}
--- libs/api/project/utils/src/index.ts
+++ libs/api/project/utils/src/index.ts
@@ -1,3 +1,2 @@
 export * from './lib/decorators/current-project.decorator';
 export * from './lib/guards/project.guard';
-export * from './lib/exceptions/http-exception.filter';
