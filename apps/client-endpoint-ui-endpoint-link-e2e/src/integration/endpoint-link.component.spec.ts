d9fb3865ae51ef184be7db60c533439b9e6d9807
Nick Thesing <nickthesing@gmail.com>
2022-02-28 18:45:31 +0100

    Fix(e2e): remove not needed test files

--- apps/client-endpoint-ui-endpoint-link-e2e/src/integration/endpoint-link.component.spec.ts
+++
@@ -1,6 +0,0 @@
-describe('client-mock-ui-endpoint-link', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=endpointlinkcomponent--primary&args=method;path;'));
-  it('should render the component', () => {
-    cy.get('ay-endpoint-link').should('exist');
-  });
-});
