745046afc0925db85718967fbf9b522246577c7e
Petar Blazhevski <petar.blazevski@gmail.com>
2022-03-02 15:43:10 +0100

    refactor response from TypeORM to Prisma

--- libs/api/response/data-access/src/lib/api-response-data-access.module.ts
+++ libs/api/response/data-access/src/lib/api-response-data-access.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ApiDatabaseDataAccessModule } from '@aymme/api/database/data-access';
 import { ResponseRepository } from './response.repository';
 import { ResponseService } from './response.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ResponseRepository])],
+  imports: [TypeOrmModule.forFeature([ResponseRepository]), ApiDatabaseDataAccessModule],
   controllers: [],
   providers: [ResponseService],
   exports: [ResponseService],
