b7b9fd09ea86bebb686d59f76d801f79d8f4481b
Nick Thesing <nickthesing@gmail.com>
2022-03-08 14:51:43 +0100

    Feat(projects): import & export

--- libs/client/mock/feature/src/lib/dialogs/index.ts
+++ libs/client/mock/feature/src/lib/dialogs/index.ts
@@ -2,3 +2,4 @@ export * from './confirm-delete-collection/confirm-delete-collection-dialog.comp
 export * from './create-new-collection/create-new-collection-dialog.component';
 export * from './rename-collection/rename-collection-dialog.component';
 export * from './project-configuration/project-configuration-dialog.component';
+export * from './import-project/import-project-dialog.component';
