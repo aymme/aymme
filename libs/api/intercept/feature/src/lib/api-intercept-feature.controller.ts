f2f46d38d15bc8112bf72f51bfcd9ed2477b7d72
Petar Blazhevski <petar.blazevski@gmail.com>
2022-03-13 20:39:17 +0100

    Revert "feat(template-variables): Response body and header should be able to parse template"

--- libs/api/intercept/feature/src/lib/api-intercept-feature.controller.ts
+++ libs/api/intercept/feature/src/lib/api-intercept-feature.controller.ts
@@ -4,7 +4,6 @@ import sleep from 'sleep-promise';
 import { Project, ProjectConfiguration } from '@prisma/client';
 import { EndpointService } from '@aymme/api/endpoint/data-access';
 import { CurrentProject, ProjectGuard } from '@aymme/api/project/utils';
-import { interpolateTemplateString } from '@aymme/api/intercept/utils';
 
 @Controller('intercept')
 export class ApiInterceptFeatureController {
@@ -40,12 +39,9 @@ export class ApiInterceptFeatureController {
       throw new NotFoundException(`Response for HTTP Status code ${endpoint.activeStatusCode} does not exist`);
     }
 
-    // TODO The variables are mocked, need to store it to DB.
-    const VARIABLES = {firstName: "Kern", lastName: "Zhao"};
-
     if (endpoint.headers && endpoint.headers.length) {
       const headers = endpoint.headers.reduce((obj, header) => {
-        return { ...obj, [interpolateTemplateString(header.name, VARIABLES)]: interpolateTemplateString(header.value, VARIABLES) };
+        return { ...obj, [header.name]: header.value };
       }, {});
       res.set(headers);
     }
@@ -53,9 +49,8 @@ export class ApiInterceptFeatureController {
     if (endpoint.delay && endpoint.delay > 10) {
       await sleep(endpoint.delay);
     }
-    const bodyAfterTemplateInterpolation = interpolateTemplateString(endpointResponse.body, VARIABLES);
 
-    return res.json(JSON.parse(bodyAfterTemplateInterpolation));
+    return res.json(JSON.parse(endpointResponse.body));
   }
 
   constructor(private endpointService: EndpointService) {}
