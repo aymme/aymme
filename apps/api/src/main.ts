c1c256245eb9b86ec9f61224566021600d192b05
Petar Blazhevski <petar.blazevski@gmail.com>
2022-03-23 21:05:29 +0100

    chore(api): add the Winston logger to the app

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,12 +6,36 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import * as bodyParser from 'body-parser';
+import * as winston from 'winston';
 
 import { PrismaService } from '@aymme/api/database/data-access';
 import { AppModule } from './app/app.module';
+import { utilities as nestWinstonModuleUtilities, WinstonModule } from 'nest-winston';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      levels: {
+        error: 1,
+        warn: 1,
+        help: 1,
+        data: 1,
+        info: 1,
+        debug: 1,
+        verbose: 1,
+      },
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.ms(),
+            nestWinstonModuleUtilities.format.nestLike('AYMME', { prettyPrint: true })
+          ),
+        }),
+        // other transports...
+      ],
+    }),
+  });
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
