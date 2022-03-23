c3fcf726b79a6e5d5890ff71952de9afa26aeb6b
Petar Blazhevski <petar.blazevski@gmail.com>
2022-03-23 20:23:40 +0100

    test(api): refactor Project Service tests

--- libs/api/project/data-access/src/lib/project.service.spec.ts
+++ libs/api/project/data-access/src/lib/project.service.spec.ts
@@ -1,15 +1,61 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { ProjectService } from './project.service';
-import { PrismaService } from '@aymme/api/database/data-access';
 import { ConflictException, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { Project } from '@prisma/client';
+import { PrismaService } from '@aymme/api/database/data-access';
+import { ProjectService } from './project.service';
+
+const EXAMPLE_PROJECT: Project = {
+  id: '8a9bedb8-f8f7-4410-ad2d-55a1c14c59dc',
+  name: 'Aymme',
+  slug: 'aymme',
+  configurationId: '7202e5e1-cfc1-44fc-ba3a-a812ef3d1c0d',
+};
+
+const EXAMPLE_PROJECTS: Project[] = [EXAMPLE_PROJECT];
+const EXAMPLE_NEW_PROJECT_NAME = 'Instagram';
+const EXAMPLE_NEW_PROJECT_SLUG = 'instagram';
+
+const prismaProjectFindManyMock = jest.fn(() => {
+  return EXAMPLE_PROJECTS;
+});
+
+const prismaProjectFindUniqueMock = jest.fn(() => {
+  return EXAMPLE_PROJECT;
+});
+
+const prismaProjectFindFirstMock = jest.fn(() => null);
+
+const prismaProjectCreateMock = jest.fn(() => EXAMPLE_PROJECT);
+const prismaProjectUpdateMock = jest.fn(() => ({
+  ...EXAMPLE_PROJECT,
+  name: EXAMPLE_NEW_PROJECT_NAME,
+  slug: EXAMPLE_NEW_PROJECT_SLUG,
+}));
 
 describe('ProjectService', () => {
   let service: ProjectService;
   let prismaService: PrismaService;
 
   beforeEach(async () => {
+    jest.clearAllMocks();
+    prismaProjectFindUniqueMock.mockImplementation(() => EXAMPLE_PROJECT);
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PrismaService, ProjectService],
+      providers: [
+        ProjectService,
+        {
+          provide: PrismaService,
+          useClass: jest.fn(() => ({
+            project: {
+              findMany: prismaProjectFindManyMock,
+              findUnique: prismaProjectFindUniqueMock,
+              findFirst: prismaProjectFindFirstMock,
+              create: prismaProjectCreateMock,
+              update: prismaProjectUpdateMock,
+            },
+          })),
+        },
+      ],
     }).compile();
 
     service = module.get<ProjectService>(ProjectService);
@@ -20,18 +66,12 @@ describe('ProjectService', () => {
     expect(service).toBeDefined();
   });
 
-  afterEach(async () => {
-    await prismaService.$disconnect();
-  });
-
   describe('getAll()', () => {
     it('should get all projects and the corresponding configuration', async () => {
-      const result = { id: '1234', name: 'Aymme', slug: 'aymme', configurationId: '1223' };
-      jest.spyOn(prismaService.project, 'findMany').mockResolvedValue([result]);
-
       const data = await service.getAll();
       expect(data.length).toBeGreaterThan(0);
-      expect(data[0].name).toEqual(result.name);
+      expect(data[0].name).toEqual(EXAMPLE_PROJECTS[0].name);
+      expect(prismaProjectFindManyMock).toHaveBeenCalledTimes(1);
     });
 
     it('should return an empty array if there are no projects found', async () => {
@@ -44,22 +84,19 @@ describe('ProjectService', () => {
 
   describe('getById()', () => {
     it('should return a project by ID', async () => {
-      jest
-        .spyOn(prismaService.project, 'findUnique')
-        .mockResolvedValue({ id: '123', name: 'Aymme', slug: 'aymme', configurationId: '324' });
+      const data = await service.getById(EXAMPLE_PROJECT.id);
 
-      const data = await service.getById('123');
-
-      expect(data.id).toEqual('123');
-      expect(data.slug).toEqual('aymme');
+      expect(data.id).toEqual(EXAMPLE_PROJECT.id);
+      expect(data.slug).toEqual(EXAMPLE_PROJECT.slug);
+      expect(prismaProjectFindUniqueMock).toHaveBeenCalledTimes(1);
     });
 
     it('should throw a NotFoundException if there are no records with the provided ID', async () => {
-      jest.spyOn(prismaService.project, 'findUnique').mockResolvedValue(null);
+      prismaProjectFindUniqueMock.mockImplementation(() => null);
       expect.assertions(1);
 
       try {
-        await service.getById('123');
+        await service.getById(EXAMPLE_PROJECT.id);
       } catch (e) {
         expect(e).toBeInstanceOf(NotFoundException);
       }
@@ -67,19 +104,16 @@ describe('ProjectService', () => {
   });
 
   describe('getBySlug()', () => {
-    it('should return a project by Slug', async () => {
-      jest
-        .spyOn(prismaService.project, 'findUnique')
-        .mockResolvedValue({ id: '123', name: 'Aymme', slug: 'aymme', configurationId: '324' });
+    it('should return a project by slug', async () => {
+      const data = await service.getBySlug(EXAMPLE_PROJECT.slug);
 
-      const data = await service.getBySlug('aymme');
-
-      expect(data.id).toEqual('123');
-      expect(data.slug).toEqual('aymme');
+      expect(data.id).toEqual(EXAMPLE_PROJECT.id);
+      expect(data.slug).toEqual(EXAMPLE_PROJECT.slug);
+      expect(prismaProjectFindUniqueMock).toHaveBeenCalledTimes(1);
     });
 
     it('should throw a NotFoundException if there are no records with the provided Slug', async () => {
-      jest.spyOn(prismaService.project, 'findUnique').mockResolvedValue(null);
+      prismaProjectFindUniqueMock.mockImplementation(() => null);
       expect.assertions(1);
 
       try {
@@ -92,21 +126,20 @@ describe('ProjectService', () => {
 
   describe('create()', () => {
     it('should create a project and return the entity', async () => {
-      const result = { id: '123', name: 'Aymme', slug: 'aymme', configurationId: '324' };
-      const name = 'Aymme';
-      jest.spyOn(prismaService.project, 'findFirst').mockResolvedValue(null);
-      jest.spyOn(prismaService.project, 'create').mockResolvedValue(result);
+      const name = EXAMPLE_PROJECT.name;
 
       const project = await service.create({ name });
+
       expect(project.name).toEqual(name);
-      expect(project.slug).toEqual(result.slug);
+      expect(project.slug).toEqual(EXAMPLE_PROJECT.slug);
+      expect(prismaProjectFindFirstMock).toHaveBeenCalledTimes(1);
+      expect(prismaProjectCreateMock).toHaveBeenCalledTimes(1);
     });
 
     it('should throw a ConflictException if the record already exists', async () => {
-      const result = { id: '123', name: 'Aymme', slug: 'aymme', configurationId: '324' };
-      const name = 'Aymme';
-      jest.spyOn(prismaService.project, 'findFirst').mockResolvedValue(result);
-      jest.spyOn(prismaService.project, 'create').mockResolvedValue(null);
+      const name = EXAMPLE_PROJECT.name;
+      prismaProjectFindFirstMock.mockImplementation(() => EXAMPLE_PROJECT);
+      prismaProjectCreateMock.mockImplementation(() => null);
 
       expect.assertions(1);
 
@@ -118,36 +151,28 @@ describe('ProjectService', () => {
     });
 
     it('should throw an error if create fails', async () => {
-      const name = 'Aymme';
-      jest.spyOn(prismaService.project, 'findFirst').mockResolvedValue(null);
-      jest.spyOn(prismaService.project, 'create').mockRejectedValue('');
+      const name = EXAMPLE_PROJECT.name;
+      prismaProjectFindFirstMock.mockImplementation(() => null);
+      prismaProjectCreateMock.mockImplementation(() => {
+        throw new InternalServerErrorException();
+      });
 
-      expect.assertions(1);
-
-      try {
-        await service.create({ name });
-      } catch (e) {
-        expect(e).toBeInstanceOf(InternalServerErrorException);
-      }
+      await expect(service.create({ name })).rejects.toThrow(InternalServerErrorException);
     });
   });
 
   describe('update()', () => {
     it('should update a Project', async () => {
-      const result = {
-        id: '123',
-        name: 'Aymme',
-        slug: 'aymme',
-        configurationId: '324',
-        configuration: { ignoreParams: '', id: '334' },
-      };
-      jest.spyOn(service, 'getById').mockResolvedValue(result);
-      jest.spyOn(prismaService.project, 'update').mockResolvedValue({ ...result, name: 'New Name', slug: 'new-name' });
+      jest.spyOn(service, 'getById').mockResolvedValue({
+        ...EXAMPLE_PROJECT,
+        configuration: { id: '492a358b-b4a7-4bab-a990-8eb0127e912c', ignoreParams: '' },
+      });
+      // jest.spyOn(prismaService.project, 'update').mockResolvedValue({ ...result, name: 'New Name', slug: 'new-name' });
 
-      const project = await service.update('123', { name: 'New Name' });
+      const project = await service.update(EXAMPLE_PROJECT.id, { name: EXAMPLE_NEW_PROJECT_NAME });
 
-      expect(project.name).toEqual('New Name');
-      expect(project.slug).toEqual('new-name');
+      expect(project.name).toEqual(EXAMPLE_NEW_PROJECT_NAME);
+      expect(project.slug).toEqual(EXAMPLE_NEW_PROJECT_SLUG);
     });
   });
 });
