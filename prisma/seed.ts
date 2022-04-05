f0c24b74d4e8ff47ca3a6e3dc548e9b154d3fc27
Petar Blazhevski <petarb@backbase.com>
2022-04-05 17:43:20 +0200

    chore: remove endpoint mock

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from '@prisma/client';
-import { endpointMocks } from './mocks/endpoint';
+import { PrismaClient } from "@prisma/client";
+// import { endpointMocks } from './mocks/endpoint';
 
 const prisma = new PrismaClient();
 
@@ -27,28 +27,28 @@ async function main() {
 
   console.log(`Created Project with ID: ${project.id} and Collection with ID: ${project.collections[0].id}`);
 
-  for (const e of endpointMocks) {
-    const endpoint = await prisma.endpoint.create({
-      data: {
-        ...e,
-        project: {
-          connect: {
-            id: project.id,
-          },
-        },
-        collection: {
-          connect: {
-            id: project.collections[0].id,
-          },
-        },
-        responses: {
-          create: [{ statusCode: 500 }],
-        },
-      },
-    });
-
-    console.log(`Created Endpoint with ID: ${endpoint.id}`);
-  }
+  // for (const e of endpointMocks) {
+  //   const endpoint = await prisma.endpoint.create({
+  //     data: {
+  //       ...e,
+  //       project: {
+  //         connect: {
+  //           id: project.id,
+  //         },
+  //       },
+  //       collection: {
+  //         connect: {
+  //           id: project.collections[0].id,
+  //         },
+  //       },
+  //       responses: {
+  //         create: [{ statusCode: 500 }],
+  //       },
+  //     },
+  //   });
+  //
+  //   console.log(`Created Endpoint with ID: ${endpoint.id}`);
+  // }
 
   console.log('Seeding finished');
 }
