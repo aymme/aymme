ab39856e3aef599cead1040d0cd6e19da22e975b
Petar Blazhevski <petarb@backbase.com>
2022-05-20 15:50:57 +0200

    chore(client): update color palette

--- tailwind.base.js
+++ tailwind.base.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   theme: {
     container: {
@@ -41,25 +43,15 @@ module.exports = {
         red: {
           500: 'rgb(239 68 68);',
         },
-        gray: {
-          50: '#F4F4F4',
-          100: '#f0f0f0',
-          200: '#C9C9C9',
-          300: '#A9A9A9',
-          400: '#2E2E30',
-          500: '#282828',
-          600: '#242424',
-          700: '#161616',
-          800: '#121212',
-          900: '#0C0C0C',
-        },
+        gray: colors.gray,
         orange: {
-          500: '#ea580c',
+          500: '#f97316',
         },
         blue: {
-          500: '#4287f5',
+          500: '#3b82f6',
+          700: '#1d4ed8',
         },
       },
     },
   },
-}
+};
